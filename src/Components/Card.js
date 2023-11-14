cec033411ee41c148a4fd13d91845f1fae73c3d0
Steve <Stevekivaamwanzia@gmail.com>
2023-11-15 01:45:33 +0300

    Third Commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,24 +2,47 @@ import React from "react";
 import App from "../App";
 
 const container = {
-    padding: "20px"
+    padding: "30px",
+    height: "400px",
+    width: "210px"
 }
 
 const buttonstyle = {
     borderRadius: "50px",
     padding: "15px 30px",
-    border: "none"
+    border: "none",
+    marginTop: "35px"
+}
+
+const image = {
+    display: "flex",
+    marginTop: "15px"
+}
+
+const imagebutton = {
+    display: "flex",
 }
 
 const Card = (props) => {
     return (
         <div style = {container}>
-            <span>
+            <div style = {image}>
                 <img src={props.logo} ></img>
-            </span>
-            <h1>{props.title}</h1>
-            <p>{props.description}</p>
-            <button style= {buttonstyle}>{props.label}</button>
+            </div>
+
+            <div style = {image}>
+                <h1>{props.title}</h1>
+            </div>
+            
+            <div style = {image}>
+                <p>{props.description}</p>
+            </div>
+
+            <div style = {imagebutton}>
+                <button style= {buttonstyle}>{props.label}</button>
+            </div>
+        
+            
         </div>
     )
 }
